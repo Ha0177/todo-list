9431293f9a58d554fc54cb9bfe38382e3e0dc58e
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-06-26 12:39:04 +0300

    Implement basic functionality for To Do List

---
+++ todo.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('ul');
+
+button.addEventListener('click', function(){
+    const inputValue = input.value;
+    const listItem = document.createElement('li');
+    listItem.textContent = inputValue;
+    list.appendChild(listItem);
+});
